9815a25228494d9a53263f11335985493bafa04a
thaintd <114746768+thaintd@users.noreply.github.com>
2024-02-24 21:36:21 +0700

    [HK2T_1001] Create Navbar

--- Views/src/App.tsx
+++ Views/src/App.tsx
@@ -1,25 +1,29 @@
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import SigninPage from "./HotelSource/Customer/pages/SigninPage";
 import SignupPage from "./HotelSource/Customer/pages/SignupPage";
 import AdminSigninPage from "./HotelSource/Admin/pages/AdminSigninPage";
+import NavBar from "./HotelSource/Customer/components/NavBar";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <PrivateRoute>
-            <></>
-          </PrivateRoute>
-        }/>
-        <Route path="/admin" Component={AdminSigninPage}/>
-        <Route path="/sign-in" Component={SigninPage}/>
-        <Route path="/sign-up" Component={SignupPage}/>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route path="/admin" Component={AdminSigninPage} />
+        <Route path="/sign-in" Component={SigninPage} />
+        <Route path="/sign-up" Component={SignupPage} />
+        <Route path="/home" Component={NavBar} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
